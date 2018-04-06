5b8bfc897991a0b5622c26f05b97a1358c28cdd1
daniel <cens097541@gmail.com>
2018-04-06 11:30:56 +0200

    initial commit

---
+++ package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+MINECRAFT_VERSION=$(cat BuildData/info.json | grep minecraftVersion | cut -d '"' -f 4)
+VANILLA_JAR=work/$MINECRAFT_VERSION/$MINECRAFT_VERSION.jar
+
+VANILLA_URL="https://s3.amazonaws.com/Minecraft.Download/versions/$MINECRAFT_VERSION/minecraft_server.$MINECRAFT_VERSION.jar"
+
+SERVER_JAR=TacoSpigot-Server/target/$(ls TacoSpigot-Server/target | grep -P "^server-[\d\.]+-[\w\.]+(-SNAPSHOT)?.jar")
+
+if [ ! -f "$SERVER_JAR" ]; then
+    echo "Server Jar: $SERVER_JAR not found"
+    exit 1;
+fi;
+
+./generateJar.sh "$SERVER_JAR" "$VANILLA_JAR" "$VANILLA_URL" "TacoSpigot"
+
+if [ $? != 0 ]; then
+    echo "Failed to generate jar"
+    exit 1
+fi;
+
+mkdir -p build
+
+cp work/Paperclip/TacoSpigot.jar build/TacoSpigot.jar
